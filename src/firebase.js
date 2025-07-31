2ef8bb0e2191071d545cf3192744c8a29a3f07a5
Khaled Rashed <khaled_abdel_razek@hotmail.com>
2025-07-31 10:53:23 +0300

    Fix Firebase environment variable configuration

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAabxrfcCTD08JRzRKmcCX1kyczwwet9cw",
-  authDomain: "future-step-nursery.firebaseapp.com",
-  projectId: "future-step-nursery",
-  storageBucket: "future-step-nursery.firebasestorage.app",
-  messagingSenderId: "583089627394",
-  appId: "1:583089627394:web:91c351bd5ac2bee3d7a21b",
-  measurementId: "G-8ZHEJSLP6J"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
